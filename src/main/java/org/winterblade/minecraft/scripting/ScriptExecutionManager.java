5401404efada16688dfbf80edee541c9bc7c9134
Matt <legendblade@users.noreply.github.com>
2016-04-14 17:17:42 -0700

    Working SEM with a logger.

--- src/main/java/org/winterblade/minecraft/scripting/ScriptExecutionManager.java
+++ src/main/java/org/winterblade/minecraft/scripting/ScriptExecutionManager.java
@@ -4,11 +4,9 @@ import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import jdk.nashorn.api.scripting.ClassFilter;
 import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
+import org.apache.logging.log4j.Logger;
 
-import javax.script.Bindings;
-import javax.script.ScriptContext;
-import javax.script.ScriptEngine;
-import javax.script.ScriptException;
+import javax.script.*;
 import java.io.IOException;
 
 /**
@@ -36,27 +34,19 @@ public class ScriptExecutionManager {
 
     /**
      * Gets a new context for your mod to use.
-     * @param identifier            Your mod's name; this will be used for any print() statements inside your scripts.
+     * @param logger                A logger that will handle messages coming from the print/console.log statements in
+     *                              your script file.
      * @return                      The configured ScriptEngine.
      */
-    public static ScriptEngine getNewContext(String identifier) {
-        return getNewContext(identifier, false);
+    public static ScriptEngine getNewContext(Logger logger) {
+        return getNewContext(logger, new String[0]);
     }
+
 
     /**
      * Gets a new context for your mod to use.
-     * @param identifier            Your mod's name; this will be used for any print() statements inside your scripts.
-     * @param allowLoad             If scripts run in your mod are allowed to load other scripts (including those from
-     *                              the internet); generally this is best left as false.
-     * @return                      The configured ScriptEngine.
-     */
-    public static ScriptEngine getNewContext(String identifier, boolean allowLoad) {
-        return getNewContext(identifier, new String[0], allowLoad);
-    }
-
-    /**
-     * Gets a new context for your mod to use.
-     * @param identifier            Your mod's name; this will be used for any print() statements inside your scripts.
+     * @param logger                A logger that will handle messages coming from the print/console.log statements in
+     *                              your script file.
      * @param allowedPackageRoots   A list of allowed packages that are able to be loaded from within scripts.
      *                              This should be limited to your mod if you allow users to edit your scripts, as
      *                              access to the Java internals in a script that's user editable could cause significant
@@ -64,43 +54,30 @@ public class ScriptExecutionManager {
      *                              your class, you may leave this blank.
      * @return                      The configured ScriptEngine.
      */
-    public static ScriptEngine getNewContext(String identifier, String[] allowedPackageRoots) {
-        return getNewContext(identifier, allowedPackageRoots, false);
-    }
-
-
-    /**
-     * Gets a new context for your mod to use.
-     * @param identifier            Your mod's name; this will be used for any print() statements inside your scripts.
-     * @param allowedPackageRoots   A list of allowed packages that are able to be loaded from within scripts.
-     *                              This should be limited to your mod if you allow users to edit your scripts, as
-     *                              access to the Java internals in a script that's user editable could cause significant
-     *                              security risks to your end users.  If you don't need to call back to packages in
-     *                              your class, you may leave this blank.
-     * @param allowLoad             If scripts run in your mod are allowed to load other scripts (including those from
-     *                              the internet); generally this is best left as false.
-     * @return                      The configured ScriptEngine.
-     */
-    public static ScriptEngine getNewContext(String identifier, String[] allowedPackageRoots, boolean allowLoad) {
+    public static ScriptEngine getNewContext(Logger logger, String[] allowedPackageRoots) {
         // TODO: Suppress warnings
         ScriptEngine nashorn = instance.factory.getScriptEngine(new ScriptExecutionSandbox(allowedPackageRoots));
         final Bindings bindings = nashorn.getBindings(ScriptContext.ENGINE_SCOPE);
 
         // Actually try and load our script header into Nashorn.
         try {
-            nashorn.eval(instance.header + "\n__nashornLibInternal.configureLogName(" + identifier + ");");
+            nashorn.eval(instance.header);
         } catch (ScriptException e) {
             System.err.println("Error processing script header file; please report this issue: " + e.getMessage());
         }
 
-        // Remove most of our bindings
+        Invocable invocable = (Invocable) nashorn;
+        try {
+            invocable.invokeFunction("__nashornLibInternalConfigureLogger", logger);
+        } catch (ScriptException | NoSuchMethodException e) {
+            System.err.println("Unable to configure logger: " + e.getMessage());
+        }
+
+        // Remove most of our bindings; in testing, we don't have access to these anyway
         bindings.remove("exit");
         bindings.remove("quit");
-
-        if(!allowLoad) {
-            bindings.remove("load");
-            bindings.remove("loadWithNewGlobal");
-        }
+        bindings.remove("load");
+        bindings.remove("loadWithNewGlobal");
 
         return nashorn;
     }
